aaafb3ab699b058b82bdb00fecbea0a17e4d9aca
pbilka46 <patryk.bilka@gmail.com>
2018-05-23 14:58:42 +0200

    Application state setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log(123)
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/lib/integration/react';
+import appState from './store';
+
+const Index = () => {
+  return <div>Hello React!</div>;
+};
+
+ReactDOM.render(
+  <Provider store={appState.store}>
+    <PersistGate persistor={appState.persistor}>
+      {/*<MuiThemeProvider>*/}
+        <Index/>
+      {/*</MuiThemeProvider>*/}
+    </PersistGate>
+  </Provider>,
+document.getElementById("root"));
---
+++ src/reducers/basicReducer.js
@@ -0,0 +1,7 @@
+
+export default function accountReducer(state = {}, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
